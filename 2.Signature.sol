a21dccb3d98ae3f86a39954c006bc659f98c227f
Asya <110995325+AsyaMaior@users.noreply.github.com>
2023-03-18 21:08:44 +0300

    Update 2.Signature.sol

--- 2.Signature.sol
+++ 2.Signature.sol
@@ -3,19 +3,25 @@
 pragma solidity ^0.8.0;
 
 contract Signature {
-
-    mapping (address => bool) list;
-    mapping (address => bool) public proposal;
-
+    
+    //'белый список': адрес => true - в белом списке, false - не в белом списке
+    mapping (address => bool) list; 
+    
+    //список подписавших: адрес => true - подписал proposal, false - не подписал proposal
+    mapping (address => bool) public proposal; 
+    
+    //проверка на принадлежность адреса к белому списку
     modifier whitelist {
-        require(list[msg.sender] == true, "You address is not in whitelist!");
+        require(list[msg.sender] == true, "You address is not in whitelist!"); 
         _;
     }
-
+    
+    //функция подписания документа (значение в proposal меняется на true)
     function signContract() public whitelist {
         proposal[msg.sender] = true;
     }
 
+    //функция добавления адреса в белый список (значение в list меняется на true)
     function addToWhitelist(address _address) public {
         list[_address] = true;
     }
