fd80c6afda01ff4bdbf8801628ec387deb1fe94c
Asya <110995325+AsyaMaior@users.noreply.github.com>
2023-03-20 11:46:21 +0300

    Update 2.Signature.sol

--- 2.Signature.sol
+++ 2.Signature.sol
@@ -3,26 +3,61 @@
 pragma solidity ^0.8.0;
 
 contract Signature {
-    
-    //'белый список': адрес => true - в белом списке, false - не в белом списке
-    mapping (address => bool) list; 
-    
-    //список подписавших: адрес => true - подписал proposal, false - не подписал proposal
-    mapping (address => bool) public proposal; 
-    
-    //проверка на принадлежность адреса к белому списку
-    modifier whitelist {
-        require(list[msg.sender] == true, "You address is not in whitelist!"); 
-        _;
-    }
-    
-    //функция подписания документа (значение в proposal меняется на true)
-    function signContract() public whitelist {
-        proposal[msg.sender] = true;
+
+    //белый лист
+    mapping (address => bool) list;
+
+    //структура отображает адрес и статус подписания им контракта (true or false)
+    struct Sign {
+        address addr;
+        bool signature;
     }
 
-    //функция добавления адреса в белый список (значение в list меняется на true)
+    //signatures - массив структур подписей
+    Sign[] public signatures;
+
+    //проверка на принадлежность адреса к белому листу
+    modifier whitelist {
+        require(list[msg.sender] == true, "You address is not in whitelist!");
+        _;
+    }
+
+    //функция подписания контракта
+    function signContract() public whitelist {
+        for (uint i = 0; i < signatures.length; i++) {
+            if (signatures[i].addr == msg.sender) {
+                signatures[i].signature = true;
+            }
+        }    
+    }
+
+    //добавить адрес в белый лист
     function addToWhitelist(address _address) public {
         list[_address] = true;
+        signatures.push(Sign(msg.sender, false));
+    }
+
+    //удалить адрес из белого листа
+    function dropFromWhitelist(address _address) public {
+        list[_address] = false;
+        for (uint i = 0; i < signatures.length; i++) {
+            if (signatures[i].addr == _address) {
+                signatures[i] = signatures[signatures.length - 1];
+                signatures.pop();
+                break;
+            }
+        }         
+    }    
+
+    //подписан ли контракт? возвращает true or false
+    function isSigned () public view returns(bool) {
+        bool _status = false;
+        for (uint i = 0; i < signatures.length; i++) {
+            if (signatures[i].signature == false) {
+                return _status;
+            }
+        }
+        _status = true;
+        return _status;
     }
 }
